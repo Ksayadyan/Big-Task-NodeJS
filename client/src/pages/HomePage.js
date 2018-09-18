d56c321b143e638b3c53ce59029f850016e0e914
davitsargsyan92 <sargsyandavit1992@gmail.com>
2018-09-18 16:14:39 +0400

    added activeStyle

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class HomePage extends Component {
+
+
+
+// componentDidMount() {
+//     fetch('/api/users')
+//       .then(res => res.json())
+//       .then(users => this.setState({users}, () => console.log('Customers fetched...', users)));
+//   }
+
+
+  render() {
+    return (
+      <div>
+          <h1>Home Page</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
